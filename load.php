e932713a1e0fb5eccd5195472f3b57c99c7082da
ajaj <109032448+mozaja@users.noreply.github.com>
2023-09-07 14:39:42 +0200

    Update load.php

--- load.php
+++ load.php
@@ -9,31 +9,5 @@ $dbname = "noticias_municip";
 // Cria uma conexão com o banco de dados
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
-// Verifica a conexão
-if (!$conn) {
-    die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
-}
-
-// Consulta SQL para recuperar notícias
-$sql = "SELECT * FROM Noticias";
-
-$result = mysqli_query($conn, $sql);
-
-// Verifica se há resultados
-if (mysqli_num_rows($result) > 0) {
-    // Loop através dos resultados e exibe as notícias
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "Título: " . $row["Titulo"] . "<br>";
-        echo "Conteúdo: " . $row["Conteudo"] . "<br>";
-        echo "Data de Publicação: " . $row["DataPublicacao"] . "<br>";
-        echo "Categoria: " . $row["Categoria"] . "<br>";
-        // Você pode exibir mais informações conforme necessário
-        echo "<hr>";
-    }
-} else {
-    echo "Nenhuma notícia encontrada.";
-}
-
-// Fecha a conexão com o banco de dados
-mysqli_close($conn);
+echo $conn;
 ?>
